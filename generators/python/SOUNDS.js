78af8de1d3e89d0ff83c27073b5d57644fbbeac0
CStevenO <carloss.ortizc@ecci.edu.co>
2020-02-29 15:27:11 -0500

    La depuracion no funciono, toca volver a dejar los archivos originales

---
+++ generators/python/SOUNDS.js
@@ -0,0 +1,13 @@
+'use strict';
+
+goog.provide('Blockly.Python.SOUNDS');
+
+goog.require('Blockly.Python');
+
+
+Blockly.Python['play_sound'] = function(block) {
+  var dropdown_value = block.getFieldValue('VALUE');
+  // TODO: Assemble Python into code variable.
+  var code = '...\n';
+  return code;
+};
