ea82441562b5f48df846fbade989ef917f6b6715
CStevenO <carloss.ortizc@ecci.edu.co>
2020-02-29 16:22:23 -0500

    Prueba

--- blocks/SOUNDS.js
+++ blocks/SOUNDS.js
@@ -6,7 +6,7 @@ goog.provide('Blockly.Constants.SOUNDS');  // deprecated, 2018 April 5
 
 goog.require('Blockly.Blocks');
 goog.require('Blockly');
-
+/*
 Blockly.Blocks['play_sound'] = {
   init: function() {
     this.appendDummyInput()
@@ -19,3 +19,32 @@ Blockly.Blocks['play_sound'] = {
  this.setHelpUrl("");
   }
 };
+*/
+Blockly.defineBlocksWithJsonArray([
+  // Block for colour picker.
+  {
+    "type": "play_sound",
+    "message0": "Play %1",
+    "args0": [
+      {
+        "type": "field_dropdown",
+        "name": "VALUE",
+        "options": [
+          ["C4", "sounds/c4.m4a"],
+          ["D4", "sounds/d4.m4a"],
+          ["E4", "sounds/e4.m4a"],
+          ["F4", "sounds/f4.m4a"],
+          ["G4", "sounds/g4.m4a"],
+          ["A5", "sounds/a5.m4a"],
+          ["B5", "sounds/b5.m4a"],
+          ["C5", "sounds/c5.m4a"]
+        ]
+      }
+    ],
+    "previousStatement": null,
+    "nextStatement": null,
+    "colour": 355,
+    "tooltip": "",
+    "helpUrl": ""
+  }
+]);
