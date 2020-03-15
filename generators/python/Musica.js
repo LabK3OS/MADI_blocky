fdb6bff5de804ec5b1d177a9c1478168d8a0c05a
CStevenO <carloss.ortizc@ecci.edu.co>
2020-03-15 18:18:41 -0500

    arreglado

--- generators/python/Musica.js
+++ generators/python/Musica.js
@@ -5,7 +5,7 @@ goog.provide('Blockly.Python.Musica');
 goog.require('Blockly.Python');
 
 
-Blockly.Python['block_type'] = function(block) {
+Blockly.Python['Musica'] = function(block) {
   var dropdown_nota = block.getFieldValue('Nota');
   var dropdown_name = block.getFieldValue('Tempo');
   var checkbox_name = block.getFieldValue('Combi') == 'TRUE';
