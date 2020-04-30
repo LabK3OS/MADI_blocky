62d127221feb2f807a70e0d590b851f073cddb4f
Steven <53093183+CStevenO@users.noreply.github.com>
2020-04-30 12:02:41 -0500

    Falta compilar y probar a ver que :S

--- generators/javascript/NEOPIXEL.js
+++ generators/javascript/NEOPIXEL.js
@@ -1,14 +1,13 @@
 'use strict';
 
-goog.provide('Blockly.JavaScript.NEO');
+goog.provide('Blockly.JavaScript.NEOPIXEL');
 
 goog.require('Blockly.JavaScript');
 
-
-Blockly.JavaScript['pin_neo'] = function(block) {
+Blockly.JavaScript['neopin'] = function(block) {
   var variable_variable = Blockly.JavaScript.variableDB_.getName(block.getFieldValue('Variable'), Blockly.Variables.NAME_TYPE);
   var dropdown_pin = block.getFieldValue('Pin');
-  var number_can = block.getFieldValue('Can');
+  var number_cantidad = block.getFieldValue('Cantidad');
   // TODO: Assemble JavaScript into code variable.
   var code = 'guardador_de_pines(' + dropdown_pin + ');\n';
   return code;
--- generators/python/NEOPIXEL.js
+++ generators/python/NEOPIXEL.js
@@ -1,21 +1,20 @@
 'use strict';
 
-goog.provide('Blockly.Python.NEO');
+goog.provide('Blockly.Python.NEOPIXEL');
 
 goog.require('Blockly.Python');
 
 
-Blockly.Python['pin_neo'] = function(block) {
+Blockly.Python['neopin'] = function(block) {
   var variable_variable = Blockly.Python.variableDB_.getName(block.getFieldValue('Variable'), Blockly.Variables.NAME_TYPE);
   var dropdown_pin = block.getFieldValue('Pin');
-  var number_can = block.getFieldValue('Can');
+  var number_cantidad = block.getFieldValue('Cantidad');
   Blockly.Python.definitions_['import_pin'] = 'from machine import Pin';
   Blockly.Python.definitions_['import_neo'] = 'from neopixel import NeoPixel';
   // TODO: Assemble Python into code variable.
-  var code = variable_variable + ' = NeoPixel(Pin(' + dropdown_pin + ', Pin.OUT),' + number_can + ')\n';
+  var code = variable_variable + ' = NeoPixel(Pin(' + dropdown_pin + ', Pin.OUT),' + number_cantidad + ')\n';
   return code;
 };
-
 
 
 //code for HEX to RGB
