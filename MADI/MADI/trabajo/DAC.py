e0f9eabb9a8d2dbc811464c55e8f6a4ac056755a
Steven <53093183+CStevenO@users.noreply.github.com>
2020-05-20 12:35:32 -0500

    DAC importante, pero nada mas se puede con la ESP32

--- MADI/MADI/trabajo/DAC.py
+++ MADI/MADI/trabajo/DAC.py
@@ -1,13 +1,25 @@
 #DAC function
 from machine import DAC #import Pin module，8bit DAC parameter range:0-255，output voltage range:0-3.3V
-dac0=DAC(Pin(0))
+dac0=DAC(Pin(25))
 
-dac0.write(128) #output 1.75V voltage
+dac0.waveform(freq,type,duration) #output 1.75V voltage
 
-#omega = frecuencia * 2 * pi
-# create a buffer containing a sine-wave, using half-word samples
-#buf = array(255 + int(254 * math.sin(2 * math.pi * omega * i / 128)) for i in range(128))
-#https://github.com/loboris/MicroPython_ESP32_psRAM_LoBo/wiki/dac
+# freq
 
-#dac = DAC(0)
-#dac.write_timed(buf, 2500 * len(buf), mode=DAC.CIRCULAR)
+# 16-32000 Hz for sine wave
+# 500-32000 Hz for noise
+# 170 - 3600 Hz for triangle
+# 170 - 7200 Hz for ramp and sawtooth
+
+
+# type
+
+# SINE,TRIANGLE,RAMP,SAWTOOTH and NOISE
+
+# duration
+
+# duracition in ms
+
+
+
+dac0.stopwave()
